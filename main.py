b91142369defa1422e054185d7dfb37db3d3d9a5
Piotr Warząchowski <108893195+PiotrWarzachowski@users.noreply.github.com>
2023-02-22 16:23:34 +0100

    Basic HTML added
    database not added yet, its basically only UI

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for
+import re
 import string
 import json
 import random
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
 url_map = json.load(open('database.json'))
@@ -12,20 +15,42 @@ url_map = json.load(open('database.json'))
 
 def generate_short_code():
     characters = string.ascii_letters + string.digits
-    short_code = ''.join(random.choice(characters) for _ in range(6))
+    short_code = ''.join(random.choice(characters) for _ in range(8))
     if short_code in url_map:
         return generate_short_code()
     return short_code
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Basic URL shortener aplication"
+    if request.method == 'POST':
+        print(request.form)
+        url = request.form['url']
+        regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        if re.match(regex, url) is None:
+            flash('The URL is not valid. Enter URL in this format : https://example.com')
+            return redirect(url_for('index'))
+        elif not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
 
-"""
-    Format for POST request to shorten:
-    JSON:
-        {'url' : "link to redirect", 'custom' : "" - if random "somestring" - if fixed value}
-"""
+        if request.form['custom_link'] != "":
+            customlink = os.environ['WEBSITE_URL'] + request.form['custom_link']
+            print(customlink)
+            return render_template('index.html', short_url=customlink)
+        else:
+            customlink = os.environ['WEBSITE_URL'] + generate_short_code()
+            print(customlink)
+            return render_template('index.html', short_url=customlink)
+        print(url)
+    
+    return render_template('index.html')
+
 
 @app.route("/shorten", methods=["POST"])
 def shorten():
@@ -45,7 +70,7 @@ def shorten():
 
     return short_url
 
-@app.route("/<short_code>")
+@app.route("/<short_code>/statistics")
 def redirect_url(short_code):
     if short_code in url_map:
         print(type(request.remote_addr))
@@ -65,13 +90,8 @@ def redirect_url(short_code):
         return redirect(url_map[short_code][0])
     else:
         return "Error: URL not found", 404
-@app.route("/get_views/<short_code>")
-def get_views(short_code):
-    if short_code in url_map:
-        amount_of_views = json.load(open('database.json'))[short_code][1]
-        return jsonify({"amount_of_views" : amount_of_views}), 200
-    else:
-        return "Error: URL not found", 404
+
+
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=1111)
